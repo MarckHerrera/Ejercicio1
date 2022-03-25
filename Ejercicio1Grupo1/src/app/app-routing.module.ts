f4f39a2268bfd0c4cac1b601e5187cf7216f642a
jeffer <jfranco-2020338@kinal.edu.gt>
2022-03-24 22:44:08 -0600

    Jefferson
    Dashboard

--- Ejercicio1Grupo1/src/app/app-routing.module.ts
+++ Ejercicio1Grupo1/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PaginaInicioComponent } from './components/pagina-inicio/pagina-inicio.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
-
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'inicio', component: PaginaInicioComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- Ejercicio1Grupo1/src/app/app.module.ts
+++ Ejercicio1Grupo1/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PaginaInicioComponent } from './components/pagina-inicio/pagina-inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 
@@ -12,7 +15,10 @@ import { RegistroComponent } from './components/registro/registro.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    NavbarComponent,
+    PaginaInicioComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
